d36366c11d62991a399feb2e637aca09aaef01ac
hala-bala <gieniopug@gmail.com>
2025-05-17 13:50:25 +0200

    Day 21 - Snake teleport to the other side

--- day-20-21/main.py
+++ day-20-21/main.py
@@ -24,16 +24,9 @@ screen.onkey(key="Right", fun=snake.right)
 
 screen.update()
 game_is_on = True
-while game_is_on:
-    sleep(SLEEP_TIME)
-    snake.move()
-    screen.update()
-    if snake.head.distance(food) < 10:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-    head_x = snake.head.xcor()
-    head_y = snake.head.ycor()
+
+
+def move_snake_to_other_side(head_x: int, head_y: int) -> None:
     if head_x > 300:
         snake.head.setx(-300)
     elif head_x < -300:
@@ -42,6 +35,17 @@ while game_is_on:
         snake.head.sety(-300)
     elif head_y < -300:
         snake.head.sety(300)
+
+
+while game_is_on:
+    sleep(SLEEP_TIME)
+    snake.move()
+    screen.update()
+    if snake.head.distance(food) < 10:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    move_snake_to_other_side(snake.head.xcor(), snake.head.ycor())
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
